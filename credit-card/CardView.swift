debbde7ac7eca5a867df13d54297340666cb4493
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-01-07 22:41:47 +0530

    Add biometrics

--- credit-card/CardView.swift
+++ credit-card/CardView.swift
@@ -6,33 +6,43 @@
 //
 
 import SwiftUI
+import LocalAuthentication
+
 
 struct CardView: View {
 
 	@State var card : CardData
-
 	@State var isEditing = false
+	@State private var isAuthenticated = false
 
 	var addUpdateCard: (CardData) -> Void
 
-	fileprivate func itemView(heading : String, value : Binding<String>) -> some View {
+	fileprivate func itemView(heading : String, value : Binding<String>,_ type: UIKeyboardType) -> some View {
 		return HStack{
 			Text(heading)
 				.bold()
 			Spacer()
-			TextField("", text: value)
-				.multilineTextAlignment(.trailing)
-				.disabled(!isEditing)
-				.foregroundColor(isEditing ? .blue : .accentColor)
+			if isAuthenticated {
+				TextField("", text: value)
+					.multilineTextAlignment(.trailing)
+					.disabled(!isEditing)
+					.foregroundColor(isEditing ? .blue : .accentColor)
+					.keyboardType(type)
+//					https://codingwithrashid.com/how-to-limit-characters-in-ios-swiftui-textfield/
+
+			} else {
+				SecureField("", text: value)
+					.multilineTextAlignment(.trailing)
+			}
 		}
 	}
 	
-	var body: some View {
-		List {
-			itemView(heading: "Number", value: $card.number)
-			itemView(heading: "Expiration", value: $card.expiration)
-			itemView(heading: "Security Code", value: $card.cvv)
-			itemView(heading: "Name", value: $card.nickname)
+	fileprivate func getCardListView() -> some View {
+		return List {
+			itemView(heading: "Number", value: $card.number,.numberPad)
+			itemView(heading: "Expiration", value: $card.expiration, .numberPad)
+			itemView(heading: "Security Code", value: $card.cvv, .numberPad)
+			itemView(heading: "Name", value: $card.nickname, .alphabet)
 			Picker("Card Type", selection: $card.type){
 				ForEach(CardType.allCases) { pref in
 					Text(pref.rawValue)
@@ -48,5 +58,34 @@ struct CardView: View {
 				isEditing.toggle()
 			}
 		}
-    }
+	}
+	
+	var body: some View {
+		getCardListView()
+			.onAppear {
+				authenticateUser()
+			}
+	}
+
+	private func authenticateUser() {
+		let context = LAContext()
+		var error: NSError?
+
+		// Check if the device supports biometric authentication
+		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+			let reason = "Please authenticate to view your card details."
+			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+				DispatchQueue.main.async {
+					if success {
+						isAuthenticated = true
+					} else {
+						isAuthenticated = false
+					}
+				}
+			}
+		} else {
+			isAuthenticated = false
+		}
+	}
+
 }
