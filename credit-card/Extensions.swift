3869752e8bb019dd9bcd1ff8b9880135aafd64d9
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-01-07 18:38:49 +0530

    Update obsificuted password

--- credit-card/Extensions.swift
+++ credit-card/Extensions.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 extension String {
-	func toSecureCard () -> String {
-		let spaceCount = self.components(separatedBy: " ").count - 1
-		return self
-//			.replacing(try! Regex("[0-9]"), maxReplacements: self.count - (spaceCount + Int(UserSettings.shared.$showNumber.wrappedValue))) { val in
-//			"X"
-//		}
+	func toSecureCard() -> String {
+		let components = self.components(separatedBy: " ")
+		let processedComponents = components.map { component -> String in
+			if component.count > 4 {
+				let lastFour = component.suffix(4)
+				return String(repeating: "•", count: component.count - 4) + " " + lastFour
+			} else {
+				return component
+			}
+		}
+		return processedComponents.joined(separator: " ")
 	}
 }
