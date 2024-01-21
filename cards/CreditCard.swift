681ec1bc0ff0ac886268d6a69c518d7f10d3a6c7
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2024-01-21 12:15:38 +0800

    Add tipkit

--- cards/CreditCard.swift
+++ cards/CreditCard.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import TipKit
 
 @main
 struct CreditCard: App {
     var body: some Scene {
         WindowGroup {
             HomeView()
+                .task {
+                    try? Tips.configure([
+                        .displayFrequency(.immediate),
+                        .datastoreLocation(.applicationDefault)
+                    ])
+                }
         }
     }
 }
