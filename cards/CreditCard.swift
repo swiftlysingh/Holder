16b0549aa383b016752f48877902518308332201
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-02-12 18:11:01 +0530

    Update what's new

--- cards/CreditCard.swift
+++ cards/CreditCard.swift
@@ -40,19 +40,19 @@ extension CreditCard: WhatsNewCollectionProvider {
 
 	/// Declare your WhatsNew instances per version
 	var whatsNewCollection: WhatsNewCollection {
-		WhatsNew(
-			version: "1.1",
+		return [ WhatsNew(
+			version: "1.2",
 			title: "Discover What's New in Holder!",
 			features: [
 				WhatsNew.Feature(
-					image: .init(systemName: "camera.on.rectangle"),
-					title: "Snap & Add Cards 📸",
-					subtitle: "Adding your cards is now a snap away! Just point your camera, and voilà, securely stored."
+					image: .init(systemName: "cloud"),
+					title: "iCloud Sync Is Here!",
+					subtitle: "Effortlessly keep your cards in sync across all devices."
 				),
 				WhatsNew.Feature(
-					image: .init(systemName: "star.fill"),
-					title: "Rate Us With a Tap 💫",
-					subtitle: "Loving Holder? Tap to rate us! Your feedback brings smiles and helps us grow."
+					image: .init(systemName: "ipad.sizes"),
+					title: "Optimized for iPad",
+					subtitle: "Enjoy a seamless, multitasking-friendly UI, now with split view."
 				),
 				WhatsNew.Feature(
 					image: .init(systemName: "ant.fill"),
@@ -69,7 +69,38 @@ extension CreditCard: WhatsNewCollectionProvider {
 					print("Ready to explore the new features!")
 				}
 			)
-		)
+		),
+			WhatsNew(
+				version: "1.1",
+				title: "Discover What's New in Holder!",
+				features: [
+					WhatsNew.Feature(
+						image: .init(systemName: "camera.on.rectangle"),
+						title: "Snap & Add Cards 📸",
+						subtitle: "Adding your cards is now a snap away! Just point your camera, and voilà, securely stored."
+					),
+					WhatsNew.Feature(
+						image: .init(systemName: "star.fill"),
+						title: "Rate Us With a Tap 💫",
+						subtitle: "Loving Holder? Tap to rate us! Your feedback brings smiles and helps us grow."
+					),
+					WhatsNew.Feature(
+						image: .init(systemName: "ant.fill"),
+						title: "Bug Squashing Party 🐜🔨",
+						subtitle: "We threw a party for bugs, and none made it out alive. Enjoy the smoother experience!"
+					)
+				],
+				primaryAction: WhatsNew.PrimaryAction(
+					title: "Dive In 🚀",
+					backgroundColor: .accentColor,
+					foregroundColor: .white,
+					hapticFeedback: .notification(.success),
+					onDismiss: {
+						print("Ready to explore the new features!")
+					}
+				)
+			)
+		]
 
 	}
 
