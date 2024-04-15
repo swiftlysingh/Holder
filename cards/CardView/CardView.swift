bb833bbe0446a6c9ce5859974906b64ad24581b8
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-04-16 01:27:17 +0530

    Optional Auth

--- cards/CardView/CardView.swift
+++ cards/CardView/CardView.swift
@@ -119,7 +119,7 @@ struct CardView: View {
 			}
 		}
 		.onChange(of: scenePhase) {
-			if scenePhase == .background {
+			if scenePhase == .background && UserSettings.shared.isAuthEnabled {
 				DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(UserSettings.shared.authTimeout)) {
 					if scenePhase == .background {
 						self.model.$isAuthenticated.wrappedValue = false
