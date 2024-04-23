54fae8fc626e7affd3685a352aa7b330a0c1db45
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-04-24 00:27:40 +0530

    Add card network image and logic

--- cards/Extensions.swift
+++ cards/Extensions.swift
@@ -19,8 +19,32 @@ extension String {
 			return baseString + (components)
 		}
 	}
-}
+	func getCardNetwork() -> CardNetwork {
+		guard let number = UInt(self) else {return .other}
 
+		if number.firstDigits(count: 1) == 4 {
+			return .visa
+		} else if number.firstDigits(count: 2) == 34 || number.firstDigits(count: 2) == 37 {
+			return .amex
+		} else {
+			return .master
+		}
+	}
+}
+extension UInt {
+	func firstDigits(count: Int) -> UInt {
+		if self == 0 || count <= 0 {
+			return 0
+		}
+		let digits = Int(log10(Double(self)))
+		if digits + 1 <= count {  // If the number of requested digits is more than or equal to the number's total digits
+			return self
+		}
+		let divisor = UInt(pow(10.0, Double(digits - count + 1)))
+		return self / divisor
+	}
+
+}
 extension View {
 	/// Applies the given transform if the given condition evaluates to `true`.
 	/// - Parameters:
--- cards/Model/CardData.swift
+++ cards/Model/CardData.swift
@@ -15,7 +15,18 @@ struct CardData : Identifiable, Codable, Hashable {
 	var name : String
     var description: String
 	var type : CardType
+	var network: CardNetwork
 
+	init(id: UUID, number: String, cvv: String, expiration: String, name: String, description: String, type: CardType) {
+		self.id = id
+		self.number = number
+		self.cvv = cvv
+		self.expiration = expiration
+		self.name = name
+		self.description = description
+		self.type = type
+		self.network = number.getCardNetwork()
+	}
 	func toShareString() -> String {
 		return "Name: \(self.name) \nNumber: \(number) \nExpiration: \(expiration) \nSecurity Code: \(cvv)"
 	}
@@ -37,9 +48,35 @@ enum CardType: String, CaseIterable, Identifiable, Codable {
 
 }
 
+enum CardNetwork: String, CaseIterable, Identifiable, Codable {
+	var id: Self {
+		return self
+	}
+
+	case visa = "Visa"
+	case master = "Mastercard"
+	case amex = "Amex"
+	case other = "Other"
+
+}
+
 extension CardData {
 	func toData() throws -> Data {
 		let encoder = JSONEncoder()
 		return try encoder.encode(self)
+	}
+}
+
+struct OldCardData : Identifiable, Codable, Hashable {
+	var id: UUID
+	var number : String
+	var cvv : String
+	var expiration : String
+	var name : String
+	var description: String
+	var type : CardType
+
+	func transferToNewSchema() -> CardData {
+		return CardData(id: id, number: number, cvv: cvv, expiration: expiration, name: name, description: description, type: type)
 	}
 }
