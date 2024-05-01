ff72ab56a2e5fcf05bc41d7064af835c1fa84f02
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2024-05-01 09:51:58 +0530

    Update

--- cards/CreditCard.swift
+++ cards/CreditCard.swift
@@ -27,143 +27,14 @@ struct CreditCard: App {
 				.environment(
 					\.whatsNew,
 					 WhatsNewEnvironment(
-						// Specify in which way the presented WhatsNew Versions are stored.
-						// In default the `UserDefaultsWhatsNewVersionStore` is used.
 						versionStore: UserDefaultsWhatsNewVersionStore(),
-						// Pass a `WhatsNewCollectionProvider` or an array of WhatsNew instances
-						whatsNewCollection: self
+						whatsNewCollection: WNCollection()
 					 )
 				)
 				.environment(BoilerPlate(delegate: self))
 
         }
     }
-}
-
-extension CreditCard: WhatsNewCollectionProvider {
-
-	/// Declare your WhatsNew instances per version
-	var whatsNewCollection: WhatsNewCollection {
-		return [
-            WhatsNew(
-                version: "1.4",
-                title: "Discover What's New in Holder!",
-                features: [
-                    WhatsNew.Feature(
-                        image: .init(systemName: "creditcard.and.123"),
-                        title: "Network Images are here!",
-                        subtitle: "Now, it's easy to identify cards using there network!"
-                    ),
-                    WhatsNew.Feature(
-                        image: .init(systemName: "ant.fill"),
-                        title: "Bug Squashing Party 🐜🔨",
-                        subtitle: "We threw a party for bugs, and none made it out alive. Enjoy the smoother experience!"
-                    )
-                ],
-                primaryAction: WhatsNew.PrimaryAction(
-                    title: "Dive In 🚀",
-                    backgroundColor: .accentColor,
-                    foregroundColor: .white,
-                    hapticFeedback: .notification(.success),
-                    onDismiss: {
-                        print("Ready to explore the new features!")
-                    }
-                )
-            ),
-			WhatsNew(
-				version: "1.3",
-				title: "Discover What's New in Holder!",
-				features: [
-					WhatsNew.Feature(
-						image: .init(systemName: "square.and.arrow.up.fill"),
-						title: "Sharing is here",
-						subtitle: "Effortlessly share your cards with friends and family"
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "ipad.sizes"),
-						title: "Now Authentication is Optional",
-						subtitle: "For the daring, enjoy a more smooth experience with no Authentication"
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "ant.fill"),
-						title: "Bug Squashing Party 🐜🔨",
-						subtitle: "We threw a party for bugs, and none made it out alive. Enjoy the smoother experience!"
-					)
-				],
-				primaryAction: WhatsNew.PrimaryAction(
-					title: "Dive In 🚀",
-					backgroundColor: .accentColor,
-					foregroundColor: .white,
-					hapticFeedback: .notification(.success),
-					onDismiss: {
-						print("Ready to explore the new features!")
-					}
-				)
-			),
-			WhatsNew(
-				version: "1.2",
-				title: "Discover What's New in Holder!",
-				features: [
-					WhatsNew.Feature(
-						image: .init(systemName: "cloud"),
-						title: "iCloud Sync Is Here!",
-						subtitle: "Effortlessly keep your cards in sync across all devices."
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "ipad.sizes"),
-						title: "Optimized for iPad",
-						subtitle: "Enjoy a seamless, multitasking-friendly UI, now with split view."
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "ant.fill"),
-						title: "Bug Squashing Party 🐜🔨",
-						subtitle: "We threw a party for bugs, and none made it out alive. Enjoy the smoother experience!"
-					)
-				],
-				primaryAction: WhatsNew.PrimaryAction(
-					title: "Dive In 🚀",
-					backgroundColor: .accentColor,
-					foregroundColor: .white,
-					hapticFeedback: .notification(.success),
-					onDismiss: {
-						print("Ready to explore the new features!")
-					}
-				)
-			),
-			WhatsNew(
-				version: "1.1",
-				title: "Discover What's New in Holder!",
-				features: [
-					WhatsNew.Feature(
-						image: .init(systemName: "camera.on.rectangle"),
-						title: "Snap & Add Cards 📸",
-						subtitle: "Adding your cards is now a snap away! Just point your camera, and voilà, securely stored."
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "star.fill"),
-						title: "Rate Us With a Tap 💫",
-						subtitle: "Loving Holder? Tap to rate us! Your feedback brings smiles and helps us grow."
-					),
-					WhatsNew.Feature(
-						image: .init(systemName: "ant.fill"),
-						title: "Bug Squashing Party 🐜🔨",
-						subtitle: "We threw a party for bugs, and none made it out alive. Enjoy the smoother experience!"
-					)
-				],
-				primaryAction: WhatsNew.PrimaryAction(
-					title: "Dive In 🚀",
-					backgroundColor: .accentColor,
-					foregroundColor: .white,
-					hapticFeedback: .notification(.success),
-					onDismiss: {
-						print("Ready to explore the new features!")
-					}
-				)
-			)
-		]
-
-	}
-
 }
 
 extension CreditCard: BoilerPlated {
