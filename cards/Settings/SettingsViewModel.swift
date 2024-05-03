956ff14b4c7334671f9405fd35cca8d7796459ae
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2024-05-04 00:03:51 +0530

    Update

--- cards/Settings/SettingsViewModel.swift
+++ cards/Settings/SettingsViewModel.swift
@@ -7,11 +7,14 @@
 
 import Foundation
 import Settings
+import UIKit
 
 class SettingsViewModel: SettingsViewModelProtocol {
-
     var appVersion : String = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
     
     var privacyPolicy = "https://docs.google.com/document/d/1OD3foirDwAsmZ8Mp6cYJlDpUjAyDpvgX7rvzosnNQes"
 
+    var sourceCode: String? = "https://github.com/swiftlysingh/holder/"
+    
+    var windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
 }
