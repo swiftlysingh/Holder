c63bf26735f4d55a8bddd78c6f3e0473f3e699d5
Pushpinder Pal Singh <sayhi@swiftlysingh.com>
2024-11-17 13:43:20 -0800

    Add Onboarding kit

--- cards/CreditCard.swift
+++ cards/CreditCard.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import TipKit
 import WhatsNewKit
 import Boilerplate
+import OnboardingKit
 
 @main
 struct CreditCard: App {
@@ -32,6 +33,7 @@ struct CreditCard: App {
 					 )
 				)
 				.environment(BoilerPlate(delegate: self))
+				.showOnboardingIfNeeded(using: .prod)
 
         }
     }
@@ -43,4 +45,24 @@ extension CreditCard: BoilerPlated {
 		let dict = NSDictionary(contentsOfFile: path!) as? [String: AnyObject]
 		return dict!["TDeck"] as! String
 	}
+}
+
+extension OnboardingConfiguration {
+	static let prod = Self.init(privacyUrlString: "",
+								accentColor: .blue,
+								features: [
+									.init(image: Image(systemName: "lock.shield"),
+										  title: "Secure Storage",
+										  content: "Keep your card details safe with state-of-the-art encryption."),
+									.init(image: Image(systemName: "faceid"),
+										  title: "Biometric Authentication",
+										  content: "Access your cards securely using Face ID or Touch ID."),
+									.init(image: Image(systemName: "square.and.arrow.up"),
+										  title: "Easily Shareable",
+										  content: "Quickly and securely share card details with trusted contacts."),
+									.init(image: Image(systemName: "hand.raised.slash"),
+										  title: "Privacy First, Open Source",
+										  content: "Your data stays private and secure, and the app's code is open-source for transparency.")
+								]
+	)
 }
